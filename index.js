24bcebaa920e08e68d40e9cdacd4d855ea4ede2d
Manuel Moreno <manuel.moreno@televisatim.com>
2019-07-03 13:08:32 -0500

    Se sube el proyecto

---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const { buildSchema } = require('graphql')
+const express = require('express')
+const gqlMiddleware = require('express-graphql')
+const  { readFileSync } = require('fs')
+const { join } = require('path')
+const resolvers = require('./lib/resolvers')
+
+const app = express()
+const port = process.env.port || 3000
+
+// Definiendo el esquema
+const shema = buildSchema(
+    readFileSync(
+        join(__dirname, 'lib', 'schema.graphql'),
+        'utf-8'
+    )
+)
+
+app.use('/api', gqlMiddleware({
+  schema: shema,
+  rootValue: resolvers,
+  graphiql: true
+}))
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}/api`)
+})
